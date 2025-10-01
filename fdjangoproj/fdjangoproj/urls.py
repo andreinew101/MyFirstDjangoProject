3a69d04d2561b8f75ec6e186e6d570617cefa336
AndreiElumba <andreinew101@gmail.com>
2025-10-01 21:28:01 +0800

    Oct 1 Commit

--- fdjangoproj/fdjangoproj/urls.py
+++ fdjangoproj/fdjangoproj/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls.static import static
+from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
+    path('', include('systemuser.urls')),
     path('admin/', admin.site.urls),
-    #path('systemuser/', include('systemuser.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
