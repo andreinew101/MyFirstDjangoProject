906f00487c7bcb3d2608e45618dd50000b7f593a
AndreiElumba <andreinew101@gmail.com>
2025-10-02 19:12:33 +0800

    commit oct 2 2025

--- fdjangoproj/systemuser/views.py
+++ fdjangoproj/systemuser/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import SystemUser
 
 # Create your views here.
 
 def index(request):
     return render(request, 'systemuser/index.html')
+
+def userlist(request):
+    user_list = SystemUser.objects.all()
+    context = {
+        'user_list': user_list
+    }
+    return render(request, 'systemuser/userlist.html', context)
